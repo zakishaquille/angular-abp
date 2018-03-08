02776483e809feca04ca29aede8fed726149d5d8
Shaquille Demsi <zakiakbardemsi@gmail.com>
2018-03-08 16:05:24 +0700

    update base ddl in disabled

--- src/app/main/share/base-ddl.component.ts
+++ src/app/main/share/base-ddl.component.ts
@@ -107,7 +107,7 @@ export class BaseDropDownListComponent implements ControlValueAccessor, OnInit, 
     }
 
     ngOnChanges(changes: SimpleChanges) {
-        if (changes.input.currentValue !== undefined) {
+        if (changes.input !== undefined && changes.input.currentValue !== undefined) {
             this.retrieveByInput();
             if (!changes.input.firstChange) {
                 this.loadAllData();
