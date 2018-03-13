527be3ef1cb7238a851cb6f02bbbaf205320ab35
Shaquille Demsi <zakiakbardemsi@gmail.com>
2018-03-13 10:36:23 +0700

    update regex email validationservice

--- src/app/shared/common/share/validation.service.ts
+++ src/app/shared/common/share/validation.service.ts
@@ -20,7 +20,8 @@ export class ValidationService {
     }
 
     static emailCompleteValidator(input) {
-        let regex = new RegExp(/^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]+[A-Za-z0-9]([A-Za-z0-9-]+[A-Za-z0-9])?(\.([A-Za-z0-9-]{1,61}[A-Za-z0-9])?)*$/);
+        let regex = new RegExp(/^[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}$/);    //only valid when: a@aa.aa
+        // let regex = new RegExp(/^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]+[A-Za-z0-9]([A-Za-z0-9-]+[A-Za-z0-9])?(\.([A-Za-z0-9-]{1,61}[A-Za-z0-9])?)*$/);    //valid: a@aa
         if (regex.test(input.value)) {
             return null;
         } else {
