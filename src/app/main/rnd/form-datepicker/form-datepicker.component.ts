1a3882a505377ff7ff172e8876c970ab88f8054a
Shaquille Demsi <zakiakbardemsi@gmail.com>
2018-03-27 15:29:45 +0700

    update datepicker in form datepicker rnd page

--- src/app/main/rnd/form-datepicker/form-datepicker.component.ts
+++ src/app/main/rnd/form-datepicker/form-datepicker.component.ts
@@ -1,25 +1,42 @@
-import { Component, OnInit, Injector } from '@angular/core';
+import { Component, OnInit, Injector, ViewChild, ElementRef } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
-import * as moment from 'moment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './form-datepicker.component.html'
 })
-export class FormDatepickerComponent extends AppComponentBase  implements OnInit {
+export class FormDatepickerComponent extends AppComponentBase implements OnInit {
 
   datepickSelected: any;
   datetimepickSelected: any;
+  validationForm: FormGroup;
+  @ViewChild('datePickElement') datePickElement: ElementRef;
 
   constructor(
-    injector: Injector,) 
-  { 
-    super(injector)
+    injector: Injector,
+    _fb: FormBuilder,
+  ) { 
+    super(injector);
+    this.validationForm = _fb.group({
+      'datepicker': [null, Validators.required],
+      'datetimepicker': [null],
+    });
   }
 
   ngOnInit() {
   }
 
   save(): void {
-    console.log(this.datepickSelected + " "+ this.datetimepickSelected);
+    let momentDate = this.strToMoment(this.datepickSelected).calendar();
+    let momentDatetime = this.strToMoment(this.datetimepickSelected, undefined, true).calendar();
+    console.log('datepicker value: '+this.datepickSelected);
+    console.log('datepicker(mm/dd/yyyy) moment: '+momentDate);
+    console.log('datetimepicker value: '+this.datetimepickSelected);
+    console.log('datetimepicker(mm/dd/yyyy) moment: '+momentDatetime);
   }
+
+  clearDate() {
+    $(this.datePickElement.nativeElement).datepicker('update', '');
+  }
+
 }
